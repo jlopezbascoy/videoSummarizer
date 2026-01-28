8e8233ae631720061cf8c2fbd1801f80fc1583d2
jlopezbascoy <jlopezbascoy@gmail.com>
2026-01-28 18:24:13 +0100

    Agregar backend al proyecto

---
+++ backend/src/main/java/com/youtubesummarizer/backend/YouTubeSummarizerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.youtubesummarizer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeSummarizerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeSummarizerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/youtubesummarizer/backend/YouTubeSummarizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubesummarizer.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeSummarizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
